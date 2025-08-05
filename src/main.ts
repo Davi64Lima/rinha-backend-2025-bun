598bf846a3fa21fd376ceec9941c92ce5a258139
Davi Lima <devDavi64lima@gmail.com>
2025-08-05 14:04:37 -0300

    Refactor main module to implement clustering for improved performance
    - Replaced the existing server implementation in `main.ts` with a clustered approach using Node.js's `cluster` module.
    - The primary process now forks worker processes based on the number of CPU cores available.
    - Removed payment processing and summary endpoints to streamline the server's responsibilities.
    - Introduced a new structure for handling worker processes, enhancing scalability and performance.

--- src/main.ts
+++ src/main.ts
@@ -1,89 +1,18 @@
-import { serve } from "bun";
-import { PaymentRequest } from "./types";
-import { getSummary, logPayment } from "./summary";
-import { CONFIG } from "./config";
-import {ProcessorType} from './types'
+import os from 'os'
+import cluster from 'cluster'
+
+const runPrimaryProcess = () => {
+    const processesCount = os.cpus().length
+    console.log(`Primary ${process.pid} is running`);
+    console.log(`Forking server with ${processesCount} processes`);
+
+    for (let index = 0; index < processesCount; index++)
+        cluster.fork()
+}
+
+const runWorkerProcess = async () => {
+    await import('./server')
+}
 
 
-
-serve({
-  port: 9999,
-  fetch: async (req: Request) => {
-    const url = new URL(req.url);
-
-    if (req.method === "POST" && url.pathname === "/payments") {
-      try {
-        const data = await req.json() as PaymentRequest;
-
-        if (!data.correlationId || !data.amount || typeof data.amount !== "number") {
-          return new Response("Invalid payload", { status: 400 });
-        }
-
-        const payment = {
-          correlationId: data.correlationId,
-          amount: data.amount,
-          requestedAt: new Date().toISOString()
-        }
-
-
-        const response = await fetch(`${CONFIG.DEFAULT_PROCESSOR_URL}/payments`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body : JSON.stringify(payment)
-        })
-
-        if (response.ok) {
-          logPayment(new Date(payment.requestedAt).getTime(),payment.amount,ProcessorType.default)
-        }
-
-        return new Response(null, { status: 200 });
-      } catch {
-        return new Response("Malformed JSON", { status: 400 });
-      }
-    }
-
-    if (req.method === "GET" && url.pathname === "/payments-summary") {
-      const from = url.searchParams.get("from");
-      const to = url.searchParams.get("to");
-    
-      if (!from || !to) {
-        return new Response("Missing 'from' or 'to'", { status: 400 });
-      }
-    
-      const result = getSummary(from, to);
-    
-      return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-    if (req.method === "POST" && url.pathname === "/purge-payments") {
-      try {
-        Promise.all([fetch(`${CONFIG.DEFAULT_PROCESSOR_URL}/admin/purge-payments`, {
-          method: 'POST',
-          headers: {
-            'X-Rinha-Token':'123'
-          }
-        }
-        ),
-        fetch(`${CONFIG.FALLBACK_PROCESSOR_URL}/admin/purge-payments`, {
-          method: 'POST',
-          headers: {
-            'X-Rinha-Token':'123'
-          }
-        }
-        )])
-        return new Response('payments flush', { status: 202 });
-      } catch (err) {
-        return new Response('error flush payments', {status:500})
-      }
-    }
-    
-    
-
-    return new Response("Not found", { status: 404 });
-  },
-});
+cluster.isPrimary ? runPrimaryProcess() : runWorkerProcess()
